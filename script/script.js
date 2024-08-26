27e301e2dfc9f7e72ff339421d21e062c70343f5
FrancescoSisti <francescosisti61@gmail.com>
2024-08-26 17:26:10 +0200

    final commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
 console.log(`JS OK`);
+
+function creaGriglia() {
+    const griglia = document.getElementById("griglia");
+
+    // Pulisci la griglia in caso di clic ripetuti
+    griglia.innerHTML = "";
+
+    // Aggiungi la classe "visible" per attivare l'animazione
+    griglia.classList.add("visible");
+
+    for (let i = 1; i <= 100; i++) {
+        const cella = document.createElement("div");
+        cella.classList.add("cella");
+        cella.textContent = i;
+
+        cella.addEventListener("click", () => {
+            cella.style.backgroundColor = "lightblue";
+            //cella.classList.toggle("blue");
+            console.log("Hai cliccato sulla cella:", cella.textContent);
+        });
+        griglia.appendChild(cella);
+    }
+}
